3645fc68945a506d4089cd20998b227bffe480d6
turupawn <ahmed.hn.43@gmail.com>
2018-04-28 08:05:10 -0600

    first commit

---
+++ src/ejercicios1/Ejercicios.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios1;
+
+/**
+ *
+ * @author turupawn
+ */
+public class Ejercicios {
+
+    static int sumar(int x, int y) {
+        return -1;
+    }
+
+    static int restar(int x, int y) {
+        return -1;
+    }
+
+    static int multiplicar(int x, int y) {
+        return -1;
+    }
+
+    static int residuo(int x, int y) {
+        return -1;
+    }
+
+    static boolean esPar(int x) {
+        return false;
+    }
+
+    static boolean esMultiploDe3(int x) {
+        return false;
+    }
+
+    static int getMayor(int x, int y) {
+        return -1;
+    }
+
+    static boolean esMayorDeEdad(int edad) {
+        return false;
+    }
+
+    static boolean sonPares(int x, int y, int z) {
+        return false;
+    }
+
+    static int getMayorDe3(int x, int y, int z) {
+        return -1;
+    }
+
+}
